ebe2b07f07b195e8d1ce6116eda2b60777b28323
Luca Spezzano <spezzanoluca@gmail.com>
2019-01-16 14:25:37 +0100

    first commit

---
+++ animation2/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var menu = $(".menu"),
+        item1 = $(".link1"),
+        item3 = $(".link2"),
+        item3 = $(".link3"),
+        tl;
+
+
+
+    tl = new TimelineLite();
+
+    tl.to(menu, .6, { width: 250, height: "100%", top: 0, bottom: 0, borderBottomLeftRadius: 0, borderTopLeftRadius: 0 })
+
+
+
+
+
+});
