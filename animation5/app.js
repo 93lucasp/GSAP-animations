7778023368c0b8984edba50ad95e652665720a67
93lucasp <spezzanoluca@gmail.com>
2019-01-20 16:16:38 +0100

    adding animations

--- animation5/app.js
+++ animation5/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $(window).resize(function(){location.reload();});
     var dot2 = $(".container .dot2"),
         dot3 = $(".container .dot3"),
         dot4 = $(".container .dot4"),
@@ -15,18 +16,65 @@ $(document).ready(function() {
         dot17 = $(".container2 .dot8"),
         lineItem1 = $(".item-line--1"),
         link1 = $(".link1"),
+        lineItem2 = $(".item-line--2"),
+        link2 = $(".link2"),
         verticalLine1 = $(".vertical-line-1"),
+        verticalLine2 = $(".vertical-line-2"),
+        verticalLineCont2 = $(".vertical-line-cont-2"),
+        verticalLineCont3 = $(".vertical-line-cont-3"),
+        horizontalLine1 = $(".horizontal-line-1"),
+        horizontalLine2 = $(".horizontal-line-2"),
+        title1 = $(".title1"),
+        title2 = $(".title2"),
         tl1,
         tl2,
         tl3;
+        // { onComplete: reverseClick}
 
     tl3 = new TimelineLite();
-
+    var positionEl1 = $( ".item-line1" ).offset().top;
+    var windowH = $( window ).height();
+    console.log(windowH)
     $(".item--1").click(function() {
-        tl3.to(link1, .4, { opacity: 0, scale: 0, ease: Power0.easeNone })
-            .to(lineItem1, 1, { width: "100%", animationPlayState: "paused", ease: Power1.easeIn })
-            .to(verticalLine1, 1, { height: "100vh", ease: Power1.easeIn })
+        tl3.to(link1, .1, { opacity: 0, scale: 0, ease: Power0.easeNone })
+            .to(lineItem1, .2, {ease: Power0.easeNone, width: "100%" })
+            .to(verticalLine1, .4, { height: (windowH*2) -  positionEl1 })
+            .to(verticalLineCont2, .4, { height: "100vh", ease: Power1.easeIn })
+            .to(horizontalLine1, .4, { width: "100%", ease: Power1.easeIn })
+            .to(title1, .6, { opacity: 1, bottom:30,ease: Power1.easeIn },"-=0.2")
+            .to(horizontalLine1, .1, { width: 0, ease: Power1.easeIn })
+            .to(verticalLineCont2, .1, { height: 0, ease: Power1.easeIn })
+            .to(verticalLine1, .1, { height: 0, ease: Power1.easeIn })
+            .to(lineItem1, .1, {ease: Power0.easeNone,width: "0" })
+            .to(link1, .1, { scale: 1, ease: Power0.easeNone })
+            $('html, body').animate({
+                scrollTop: $('.container2').offset().top
+            }, 1000);
+            
     });
+    $(".item--2").click(function() {
+        tl3.to(link2, .1, { opacity: 0, scale: 0, ease: Power0.easeNone })
+            .to(lineItem2, .2, {ease: Power0.easeNone, width: "100%"})
+            .to(verticalLine2, .4, { height: (windowH*3) -  positionEl1, ease: Power1.easeIn })
+            .to(verticalLineCont2, .4, { height: "100vh", ease: Power1.easeIn },"-=0.12")
+            .to(verticalLineCont3, .4, { height: "100vh", ease: Power1.easeIn },"-=0.12")
+            .to(horizontalLine2, .4, { width: "100%", ease: Power1.easeIn })
+            .to(title2, .6, { opacity: 1, bottom:30,ease: Power1.easeIn },"-=0.2")
+            .to(horizontalLine2, .1, { width: 0, ease: Power1.easeIn })
+            .to(verticalLineCont3, .1, { height: 0, ease: Power1.easeIn })
+            .to(verticalLineCont2, .1, { height: 0, ease: Power1.easeIn })
+            .to(verticalLine2, .1, { height: 0, ease: Power1.easeIn })
+            .to(lineItem2, .1, {ease: Power0.easeNone, width: "0"})
+            .to(link2, .1, { scale: 1, ease: Power0.easeNone })
+            $('html, body').animate({
+                scrollTop: $('.container3').offset().top
+            }, 1200);
+            
+    });
+    
+    // function reverseClick() {
+    //     tl3.reverse();
+    // }
     tl1 = new TimelineLite({ onComplete: reverse1, onReverseComplete: restart1 });
     tl1.to(dot2, 0, { opacity: 1 })
         .to(dot2, 1, { top: "98.6%", ease: Power0.easeNone })
